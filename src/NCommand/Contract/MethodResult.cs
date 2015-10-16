2f333e02cba611d2cb70e16a4459413378c17cc9
reti <etienne.ribeiro@tectil.ch>
2015-10-16 10:50:56 +0200

    Added missing files.

---
+++ src/NCommand/Contract/MethodResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tectil.NCommand.Contract
+{
+    /// <summary>
+    /// Method result
+    /// </summary>
+    /// <typeparam name="T">Method attribute</typeparam>
+    public class MethodResult<T>
+    {
+        /// <summary>
+        /// Gets or sets the methodes.
+        /// </summary>
+        /// <value>
+        /// The methodes.
+        /// </value>
+        public List<MethodeAttribute<T>> Methodes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the exceptions.
+        /// </summary>
+        /// <value>
+        /// The exceptions.
+        /// </value>
+        public List<Exception> Exceptions { get; set; }
+    }
+}
---
+++ src/NCommand/Contract/MethodeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace Tectil.NCommand.Contract
+{
+    /// <summary>
+    /// Methode and attribute
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MethodeAttribute<T>
+    {
+        /// <summary>
+        /// Gets or sets the method information.
+        /// </summary>
+        /// <value>
+        /// The method information.
+        /// </value>
+        public MethodInfo MethodInfo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the attribute.
+        /// </summary>
+        /// <value>
+        /// The attribute.
+        /// </value>
+        public T Attribute { get; set; }
+    }
+}
