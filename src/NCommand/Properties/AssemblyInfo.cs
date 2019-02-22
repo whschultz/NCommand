57a55a6ef79507765daf1e76d04a2d95548b2e61
Hank Schultz <whschultz@mac.com>
2019-02-22 14:03:35 -0500

    Converted projects to new Visual Studio 2017 format.

--- src/NCommand/Properties/AssemblyInfo.cs
+++ src/NCommand/Properties/AssemblyInfo.cs
@@ -1,11 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("NCommand")]
-[assembly: AssemblyDescription("NCommand is a .NET library to parse command line arguments and map them to commands.")]
-[assembly: AssemblyCompany("Tectil GmbH")]
-[assembly: AssemblyProduct("NCommand")]
-[assembly: AssemblyCopyright("Copyright © Etienne Ribeiro, 2015")]
-[assembly: AssemblyVersion("0.1.*")]
 [assembly: InternalsVisibleTo("NCommand.Test")]
 
